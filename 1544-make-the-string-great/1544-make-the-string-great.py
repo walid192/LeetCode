bfb710a43cf9e5d8d538d8236aace40948086d7c
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-04-05 23:50:34 +0100

    Time: 8 ms (97.16%), Space: 11.5 MB (89.57%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if s=="":
-            return s
-        i = 0
-        while i < len(s) - 1:
-            if s[i] != s[i+1] and (s[i].lower() == s[i+1] or s[i].upper() == s[i+1]):
-                s = s[:i] + s[i+2:]
-                return self.makeGood(s)  
-            else:
-                i += 1
+        stack=[]
         
-        return s
+        for i in range(len(s)):
+            if(stack and abs(ord(stack[-1])-ord(s[i]))==32):
+                stack.pop()
+            else:
+                stack.append(s[i])
+            
+        return ''.join(stack)
             
         
