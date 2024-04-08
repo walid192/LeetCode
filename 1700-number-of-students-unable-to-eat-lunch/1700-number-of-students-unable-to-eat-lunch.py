0a28a31f806ee086b6f0b738eb3ff0ff9b684e46
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-04-08 23:47:31 +0100

    Time: 15 ms (87.02%), Space: 11.5 MB (80.53%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -8,24 +8,21 @@ class Solution(object):
         count_zero_students = students.count(0)
         count_one_students = len(students) - count_zero_students
         
-        # Initialize variables to keep track of how many students of each type have taken sandwiches
         count_zero_taken = 0
         count_one_taken = 0
         
-        # Simulate students taking sandwiches or going to the end of the queue
         while sandwiches:
-            if sandwiches[0] == 0:  # Circular sandwich
+            if sandwiches[0] == 0: 
                 if count_zero_taken < count_zero_students:
                     count_zero_taken += 1
                     sandwiches.pop(0)
                 else:
                     break
-            else:  # Square sandwich
+            else:  
                 if count_one_taken < count_one_students:
                     count_one_taken += 1
                     sandwiches.pop(0)
                 else:
                     break
         
-        # Return the number of students unable to eat (remaining in the queue)
         return len(sandwiches)
