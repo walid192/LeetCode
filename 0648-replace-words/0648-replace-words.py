d837b81509903bef9c07f13c1daf6452e6a83c56
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-06-07 20:55:59 +0100

    Time: 115 ms (86.07%), Space: 52.2 MB (19.67%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,3 +1,30 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+            
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def replace(self, word):
+        node = self.root
+        for i, char in enumerate(word):
+            if char not in node.children:
+                return word
+            node = node.children[char]
+            if node.is_end_of_word:
+                return word[:i + 1]
+        return word
+
 class Solution(object):
     def replaceWords(self, dictionary, sentence):
         """
@@ -5,23 +32,7 @@ class Solution(object):
         :type sentence: str
         :rtype: str
         """
-        def subWord(s,word):
-            if(len(s)>len(word)):
-                return False
-            for i in range(len(s)):
-                if(s[i]==word[i]):
-                    pass
-                else:
-                    return False
-
-            return True
-
-
-        words=sentence.split(' ')
-        for i in range(len(words)):
-            for root in dictionary:
-                if(subWord(root,words[i])):
-                    words[i]=root
-
-
-        return ' '.join(words)
+        trie = Trie()
+        for root in dictionary:
+            trie.insert(root)
+        return ' '.join(map(trie.replace, sentence.split()))
