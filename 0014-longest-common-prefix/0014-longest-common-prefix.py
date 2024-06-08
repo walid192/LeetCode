d53104da326c35b28cddaed1331a055c145662cb
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-06-08 22:36:07 +0100

    Time: 33 ms (6.34%), Space: 11.9 MB (25.25%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,3 +1,29 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def longest_common_prefix(self):
+        node = self.root
+        prefix = []
+        while node and len(node.children) == 1 and not node.is_end_of_word:
+            char, next_node = next(iter(node.children.items()))
+            prefix.append(char)
+            node = next_node
+        return ''.join(prefix)
+
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
@@ -5,18 +31,12 @@ class Solution(object):
         :rtype: str
         """
         
-        def startWith(word,pref):
-            l=len(pref)
-            return word[:l]==pref
+        if not strs:
+            return ""
 
-        prefLength=0
-        while prefLength<len(strs[0]):
-            prefix=strs[0][:prefLength+1]
-            for word in strs:
-                if not startWith(word,prefix):
-                    return strs[0][:prefLength]
+        trie = Trie()
+        for word in strs:
+            trie.insert(word)
 
-            prefLength+=1
-
-        return strs[0][:prefLength]
+        return trie.longest_common_prefix()
         
