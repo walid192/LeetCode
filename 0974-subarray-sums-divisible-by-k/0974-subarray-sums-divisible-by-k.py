d0e966bda836fc644e01a33fccb7a70e923d005d
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-06-09 22:23:30 +0100

    Time: 214 ms (85.04%), Space: 15 MB (42.52%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -5,24 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        def calculComb(n):
-            return n*(n-1)/2
-
-        total=[]
-        res=0
-        for i,num in enumerate(nums):
-            res+=num
-            total.append(res%k)
-
-        count=Counter(total)
-        nb=0
-        for c in count:
-            if c!=0:
-                 nb+=calculComb(count[c])
+        count = 0
+        prefix_sum = 0
+        prefix_map = {0: 1}  
+        
+        for num in nums:
+            prefix_sum += num
+            mod = prefix_sum % k
+            if mod < 0:  
+                mod += k
+            if mod in prefix_map:
+                count += prefix_map[mod]
+                prefix_map[mod] += 1
             else:
-                 nb+=calculComb(count[c])+count[c]
-             
-        return int(nb) 
+                prefix_map[mod] = 1
+        
+        return count
         
             
         
