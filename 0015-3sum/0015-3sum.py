ef2efe9568a46b074f2244abc59eef205a7ec164
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-06-12 10:10:30 +0100

    Time: 875 ms (57.51%), Space: 14.3 MB (99.69%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -9,7 +9,7 @@ class Solution(object):
 
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicates
+                continue 
 
             left, right = i + 1, len(nums) - 1
 
@@ -20,9 +20,9 @@ class Solution(object):
                     res.append([nums[i], nums[left], nums[right]])
 
                     while left < right and nums[left] == nums[left + 1]:
-                        left += 1  # Skip duplicates
+                        left += 1 
                     while left < right and nums[right] == nums[right - 1]:
-                        right -= 1  # Skip duplicates
+                        right -= 1 
 
                     left += 1
                     right -= 1
