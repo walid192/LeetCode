f1861fc057ad695f08b0169de336b923ea292510
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-12-02 13:28:24 +0100

    Time: 0 ms (100.00%), Space: 12.1 MB (5.80%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.py
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.py
@@ -5,19 +5,13 @@ class Solution(object):
         :type words: List[str]
         :rtype: bool
         """
-        l=len(s)
-        current_l=0
-        i=0
-        while(current_l<l and i <len(words)):
-            current_l+=len(words[i])
-            i+=1
+        res=""
+        for word in words:
+            res+=word
+            if(res==s):
+                return True
+            if(not s.startswith(res)):
+                return False
         
-        if current_l!=l:
-            return False
-            
-            
-        result = "".join(words)
-        if result.startswith(s) and len(s)>=len(words[0]):
-            return True
         return False
         
