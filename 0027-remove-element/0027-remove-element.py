b8fb31a6bd042d07e8a8ec7e230473d3e649ce75
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-12-10 16:27:01 +0100

    Time: 0 ms (100.00%), Space: 17.5 MB (6.33%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        k=0
+from collections import Counter
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0 
         for i in range(len(nums)):
-            if(nums[i]!=val):
-                nums[k]=nums[i]
-                k+=1
-        return k
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
         
