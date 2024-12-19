169c70cb73e8c20bb7513b27effe269f076eada2
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-12-19 19:56:37 +0100

    Time: 0 ms (100.00%), Space: 17.9 MB (14.45%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -3,17 +3,12 @@ class Solution:
         if len(arr) <= 1:
             return 1
 
-        sorted_arr = sorted(arr)
-        chunks = 0
-
-        running_sum_original = 0
-        running_sum_sorted = 0
-
-        for i in range(len(arr)):
-            running_sum_original += arr[i]
-            running_sum_sorted += sorted_arr[i]
-
-            if running_sum_original == running_sum_sorted:
-                chunks += 1
-
+        chunks=0
+        maxi=float('-inf')
+        
+        for i,num in enumerate(arr):
+            maxi=max(maxi,num)
+            if i==maxi:
+                chunks+=1
+                
         return chunks
