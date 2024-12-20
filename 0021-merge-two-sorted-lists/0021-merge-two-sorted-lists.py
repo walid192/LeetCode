9858949c29280aa8cc04cc23cb8bb094b35683d0
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-12-20 14:03:26 +0100

    Time: 0 ms (100.00%), Space: 17.9 MB (8.83%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,29 +1,33 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        dummy = ListNode(0)  
-        cur = dummy  
-        while l1 and l2:  
-            if l1.val <= l2.val:  
-                cur.next = l1  
-                l1 = l1.next  
-            else:  
-                cur.next = l2  
-                l2 = l2.next  
-            cur = cur.next  
-        cur.next = l1 or l2  
-  
-        return dummy.next 
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy=ListNode()
+        current=dummy
+        
+        while(l1 or l2):
+            if (l1 and l2 and l1.val<=l2.val):
+                current.next=ListNode(l1.val)
+                l1=l1.next
+            elif(l1 and l2 and l1.val>l2.val):
+                current.next=ListNode(l2.val)
+                l2=l2.next
+            elif (l1):
+                current.next=l1
+                l1=None
+            else:
+                current.next=l2
+                l2=None
+                
+            current=current.next
+            
+        return dummy.next
+                
+                
                 
             
-                
         
