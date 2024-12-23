d54034bb4212c94547f7475e80a02e4b4966765e
Walid Sboui <91627303+walid192@users.noreply.github.com>
2024-12-23 22:51:27 +0100

    Time: 0 ms (100.00%), Space: 18 MB (5.53%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,47 @@
+from collections import defaultdict, deque
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
+        
+        #create graph
+        graph=defaultdict(dict)
+        
+        for (a,b),value in zip(equations,values):
+            graph[a][b]=value
+            graph[b][a]=1/value
+            
+            
+        def bfs(start, end):
+            if start not in graph or end not in graph:
+                return -1.0
+            
+            queue = deque([(start, 1.0)])
+            visited = set()
+            
+            while queue:
+                current, product = queue.popleft()
+                
+                if current == end:
+                    return product
+                
+                visited.add(current)
+                
+                for neighbor, value in graph[current].items():
+                    if neighbor not in visited:
+                        queue.append((neighbor, product * value))
+            
+            return -1.0
+        
+        results = []
+        for C, D in queries:
+            results.append(bfs(C, D))
+        
+        return results
+            
+                
+                
+                    
+            
+            
+        
+        
