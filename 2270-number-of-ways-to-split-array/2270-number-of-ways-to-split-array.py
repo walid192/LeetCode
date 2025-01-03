148221536a9a444f2199e5ffd15fd85146b8eb8f
Walid Sboui <91627303+walid192@users.noreply.github.com>
2025-01-03 14:30:04 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -6,14 +6,13 @@ class Solution:
         pref=[0]*(len(nums)-1)
         pref[0]=nums[0]
 
-        for i in range(1,len(pref)):
-            pref[i]=pref[i-1]+nums[i]
-
         s=sum(nums)
         cnt=0
-        for i in range(len(pref)):
-            if (2*pref[i]>=s):
-                cnt+=1
+        for i in range(1,len(pref)):
+            pref[i]=pref[i-1]+nums[i]
+            cnt+=1 if (2*pref[i]>=s) else 0
+
+        
         return cnt
 
 
