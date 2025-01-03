8c934b2139d3249007fcda38d8117ed81d734595
Walid Sboui <91627303+walid192@users.noreply.github.com>
2025-01-03 15:34:39 +0100

    Time: 61 ms (64.71%), Space: 18 MB (21.85%) - LeetHub

--- 2546-number-of-subarrays-with-gcd-equal-to-k/2546-number-of-subarrays-with-gcd-equal-to-k.py
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/2546-number-of-subarrays-with-gcd-equal-to-k.py
@@ -3,32 +3,15 @@ from functools import reduce
 
 class Solution:
     def subarrayGCD(self, nums: List[int], k: int) -> int:
-        if not nums:
-            return 0
-        
         cnt = 0
-        # First loop to count single-element subarrays
-        for num in nums:
-            if math.gcd(num, k) == k:  # Fix: Added k as the second argument
-                cnt += 1
+
+        for i in range(len(nums)):
+            current_gcd = 0
+            for j in range(i, len(nums)):
+                current_gcd = math.gcd(current_gcd, nums[j])
+                if current_gcd == k:
+                    cnt += 1
+                elif current_gcd < k:
+                    break
         
-        left, right = 0, 1
-        while right < len(nums):
-            result = reduce(math.gcd, nums[left:right+1])  # Fix: Corrected the use of reduce
-            if result == k:
-                print(nums[left:right+1])  # Debug print statement
-                cnt += 1
-                right += 1
-            elif result > k:
-                right += 1
-            else:
-                left += 1
-
-            if left == right:  # Prevent infinite loop
-                right += 1
-
         return cnt
-            
-            
-            
-
